65f72300115a9996794f155754ac2922d3056261
loza1alexandra <loza1alexandra@gmail.com>
2018-09-21 11:00:50 +0300

    Сценарий логина

--- sqa/src/test/java/ru/stqa/sqa/TestLogin.java
+++ sqa/src/test/java/ru/stqa/sqa/TestLogin.java
@@ -4,14 +4,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class TestLogin {
     private WebDriver driver;
-    private WebDriverWait wait;
+
 
     @Before
     public void start () {
