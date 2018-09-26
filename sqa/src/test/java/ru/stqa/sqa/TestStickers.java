c5bc23d82a6a640724d1da99ac58320a1c540f46
loza1alexandra <loza1alexandra@gmail.com>
2018-09-26 20:02:51 +0300

    Задание 8

---
+++ sqa/src/test/java/ru/stqa/sqa/TestStickers.java
@@ -0,0 +1,47 @@
+package ru.stqa.sqa;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.assertFalse;
+
+
+
+public class TestStickers {
+    private WebDriver driver;
+
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+
+
+    }
+
+    boolean areElementsPresent;
+
+
+
+    @Test
+    public void testStickers() {
+        driver.get("http://localhost/litecart/");
+
+        for (WebElement d : driver.findElements(By.cssSelector(".link[title*=Duck]"))) {
+
+            if (d.findElements(By.cssSelector("div.sticker")).size() == 1) {
+                areElementsPresent = true;
+            } else {
+                areElementsPresent = false;
+            }
+            assertFalse(!areElementsPresent);
+        }
+    }
+
+        @After
+        public void stop() {
+            driver.quit();
+            driver = null;
+        }
+    }
