fec9a35c581644c830600d188c89c9c96e035001
loza1alexandra <loza1alexandra@gmail.com>
2018-10-03 15:00:30 +0300

    Правки по заданию 7

--- sqa/src/test/java/ru/stqa/sqa/TestMenu.java
+++ sqa/src/test/java/ru/stqa/sqa/TestMenu.java
@@ -8,37 +8,33 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.ArrayList;
+
+import java.util.*;
+
+
 
 import static org.junit.Assert.assertFalse;
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 
 public class TestMenu {
     private WebDriver driver;
-    //private WebDriverWait wait;
-    private WebElement menu;
 
 
     @Before
     public void start () {
         driver = new ChromeDriver();
-        //wait = new WebDriverWait(driver, 10);
-
-
 
     }
 
-    public boolean isElementPresent (By locator) {
+   public boolean isElementPresent (By locator) {
         try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException ex) {
+           driver.findElement(locator);
+           return true;
+      } catch (NoSuchElementException ex) {
             return false;
-        }
-    }
+       }
+   }
 
 
 
@@ -48,37 +44,34 @@ public class TestMenu {
         driver.findElement(By.cssSelector("[name=username]")).sendKeys("admin");
         driver.findElement(By.cssSelector("[type=password]")).sendKeys("123456");
         driver.findElement(By.cssSelector("[type=submit]")).click();
-        String s = driver.findElement(By.cssSelector(".name")).getText();
-        //ArrayList<WebElement> m = new ArrayList<>(driver.findElements(By.cssSelector(".name")));
-    while (s == "vQmods") {
-        //ArrayList<WebElement> m = new ArrayList<>(driver.findElements(By.cssSelector(".name")));
-        menu = driver.findElement(By.cssSelector(".name"));
-        driver.findElement(By.cssSelector(".selected"))
-        menu.click();
+        int x = driver.findElements(By.cssSelector("li#app-")).size();
+        for (int i=0; i< x; i++) {
+            ArrayList<WebElement> menu = new ArrayList<>(driver.findElements(By.cssSelector("li#app-")));
+            WebElement app = menu.get(i);
+            app.click();
+            assertFalse(!isElementPresent(By.cssSelector("h1")));
+            if (driver.findElements(By.cssSelector(".docs")).size() > 0) {
+                menu = new ArrayList<>(driver.findElements(By.cssSelector("li#app-")));
+                app = menu.get(i);
+                int y = app.findElements(By.cssSelector("li")).size();
+                for (int j = 1; j < y; j++) {
+                    menu = new ArrayList<>(driver.findElements(By.cssSelector("li#app-")));
+                    app = menu.get(i);
+                    ArrayList<WebElement> m = new ArrayList<>(app.findElements(By.cssSelector("li")));
+                    WebElement a = m.get(j);
+                    a.click();
+                    assertFalse(!isElementPresent(By.cssSelector("h1")));
+                    menu = new ArrayList<>(driver.findElements(By.cssSelector("li#app-")));
+                    menu.get(i);
+                }
+            }
 
-    }
-        //int i = 0;
-        //driver.findElements(By.cssSelector(".name"));
-        String s = driver.findElement(By.cssSelector(".name")).getText();
-        System.out.println(s);
-        //driver.findElement(By.cssSelector(".name")).click();
-        //assertFalse(isElementPresent(By.cssSelector("h1")));
 
-        //for (WebElement menu : driver.findElements(By.cssSelector(".name"))) {
-           //menu.click();
-            //wait.until(titleIs("webinar - Поиск в Google"))
-            //if (menu.findElements(By.cssSelector("h1")).size() > 0) {
-                //areElementsPresent = true;
-            //} else {
-                //areElementsPresent = false;
-            //}
-            //assertFalse(!areElementsPresent);
+            }
         }
 
-        //}
-        //driver.findElement(By.cssSelector("a[href*=appearance]")).click();
-        //wait.until(titleIs("Template | My Store"));
-        //assertTrue(isElementPresent(By.cssSelector("h1")));
+
+
 
 
 
